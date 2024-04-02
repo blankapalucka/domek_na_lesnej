addcc81172e6a67c8495894680fa4c7fd48e24e7
Blanka Pałucka <blankapalucka@gmail.com>
2024-04-02 21:20:59 +0200

    final changes

--- JS/script.js
+++ JS/script.js
@@ -25,7 +25,23 @@
 
 // initMap();
 
-function myFunction() { console.log("działa") 
-document.getElementById("myDropdown").classList.toggle("show");
+function myFunction() {
+    console.log("działa")
+    document.getElementById("myDropdown").classList.toggle("show");
 
 }
+
+
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function (event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
