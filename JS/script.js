d913282c79ee4671654f8b1347c31bf8e54a53de
Blanka Pałucka <blankapalucka@gmail.com>
2024-04-18 22:02:44 +0200

    Hamburger menu apears slowly. You can click outside to close it

--- JS/script.js
+++ JS/script.js
@@ -25,23 +25,11 @@
 
 // initMap();
 
-function myFunction() {
-    console.log("działa")
-    document.getElementById("myDropdown").classList.toggle("show");
-
+// // Open/Close the dropdown menu if the user clicks on it or outside of it
+window.onclick = function (event) {
+    if (event.target.matches('.hamburger')) {
+        document.getElementById("myDropdown").classList.toggle("show");
+    } else {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
 }
-
-
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
